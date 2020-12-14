f6e2445050ddba2a1418394879a60505198692f5
XL P <921417536@qq.com>
2020-12-14 11:28:19 +0800

    commit 12-14

--- src/views/Publisher/AppManage.vue
+++ src/views/Publisher/AppManage.vue
@@ -3,7 +3,7 @@
     <a-tabs default-active-key="statisics">
       <a-tab-pane key="statisics" tab="统计信息">
         <a-skeleton :loading="loading" active>
-            <a-statistic title="销量" :value="AppInfo.volume" />
+            <a-statistic title="销量" :value="AppInfo.mainPlan.volume" />
         </a-skeleton>
       </a-tab-pane>
 
@@ -93,16 +93,53 @@
                 </a-form-item>
             
                 <a-form-item label="订阅项目">
-                    <a-table :columns="planTableCols" :data-source="AppInfo.subscribePlans" :loading="loading" :pagination="false" @change="pageChanged">
-                        <template #durationSlot="duration"> {{duration/(24*60*60)}}天 </template>
-                        <template #activeSlot="active"> 
-                            <span v-if="active">可用</span>
-                            <span v-else>禁用</span>
+                    <a-table :columns="planTableCols" :data-source="AppInfo.subscribePlans" :loading="loading" :pagination="false">
+                        <template #codeSlot="code, item"> 
+                            <a-input v-if="item.editing" v-model="planEditInfo.newInfo.code" /> <!--编辑模式-->
+                            <span v-else> {{code}} </span> <!--正常显示-->
                         </template>
-                        <template #actionSlot="">
-                            <a-button type="link" style="padding:0px">编辑</a-button><a-divider type="vertical" />
-                            <a-button type="link" style="padding:0px">禁用</a-button><a-divider type="vertical" />
-                            <a-button type="link" style="padding:0px; color:red">删除</a-button>
+
+                        <template #nameSlot="name, item"> 
+                            <a-input v-if="item.editing" v-model="planEditInfo.newInfo.name" /> <!--编辑模式-->
+                            <span v-else> {{name}} </span> <!--正常显示-->
+                        </template>
+
+                        <template #explanationSlot="explanation, item"> 
+                            <a-textarea v-if="item.editing" v-model="planEditInfo.newInfo.explanation" /> <!--编辑模式-->
+                            <span v-else> {{explanation}} </span> <!--正常显示-->
+                        </template>
+
+                        <template #priceSlot="price, item"> 
+                            <a-input-number v-if="item.editing" v-model="planEditInfo.newInfo.price" /> <!--编辑模式-->
+                            <span v-else> {{price}} </span> <!--正常显示-->
+                        </template>
+
+                        <template #oripriceSlot="oriprice, item"> 
+                            <a-input-number v-if="item.editing" v-model="planEditInfo.newInfo.oriprice" /> <!--编辑模式-->
+                            <span v-else> {{oriprice}} </span> <!--正常显示-->
+                        </template>
+
+                        <template #durationSlot="duration, item"> 
+                            <a-input-number v-if="item.editing" v-model="planEditInfo.newInfo.duration" /> <!--编辑模式-->
+                            <span v-else> {{duration}} </span> <!--正常显示-->
+                        </template>
+
+                        <template #activeSlot="active, item"> 
+                            <a-switch v-if="item.editing" v-model="planEditInfo.newInfo.active" /> <!--编辑模式-->
+                            <span v-else> <!--正常显示-->
+                                <span v-if="active">可用</span>
+                                <span v-else>禁用</span> 
+                            </span> 
+                        </template>
+                        <template #actionSlot="item">
+                            <template v-if="item.editing"><!--编辑模式-->
+                                <a-button type="link" size="small" @click="planEditSaveClicked(item.key)">保存</a-button>
+                                <a-button type="link" size="small" @click="planEditCancelClicked(item.key)">取消</a-button>
+                            </template>
+                            <template v-else><!--正常显示-->
+                                <a-button :disabled="planEditInfo.editing" type="link" style="padding:0px" @click="planEditClicked(item.key)">编辑</a-button><a-divider type="vertical" />
+                                <a-button :disabled="planEditInfo.editing" type="link" style="padding:0px">删除</a-button>
+                            </template>
                         </template>
                     </a-table>
                 </a-form-item>
@@ -136,33 +173,39 @@
 <script>
 import axios from 'axios';
 import { message } from 'ant-design-vue';
+import qs from 'qs'
 
 //Plan表格的列
 const planTableCols = [
   {
     title: '标识符',
     dataIndex: 'code',
-    key: 'code'
+    key: 'code',
+    scopedSlots: { customRender: 'codeSlot' },
   },
   {
     title: '名字',
     dataIndex: 'name',
     key: 'name',
+    scopedSlots: { customRender: 'nameSlot' },
   },
   {
     title: '说明',
     dataIndex: 'explanation',
     key: 'explanation',
+    scopedSlots: { customRender: 'explanationSlot' },
   },
   {
     title: '价格',
     key: 'price',
     dataIndex: 'price',
+    scopedSlots: { customRender: 'priceSlot' },
   },
   {
     title: '原价',
     key: 'oriprice',
     dataIndex: 'oriprice',
+    scopedSlots: { customRender: 'oripriceSlot' },
   },
   {
     title: '时长',
@@ -173,6 +216,7 @@ const planTableCols = [
   {
     title: '生效',
     key: 'active',
+    dataIndex: 'active',
     scopedSlots: { customRender: 'activeSlot' },
   },
   {
@@ -191,7 +235,11 @@ export default {
             AppInfo: undefined,
             loading: true,
             newTag: undefined,
-            planTableCols
+            planTableCols,
+            planEditInfo: {
+                editing: false, //有行正在编辑。此次其它的行禁用
+                newInfo: {}
+            }
         }
     },
     methods: {
@@ -239,6 +287,10 @@ export default {
             }).then(res=>{
                 if (res.data.success) {
                     this.AppInfo = res.data.detail;
+                    for (let i=0; i<this.AppInfo.subscribePlans.length; ++i) {
+                        this.AppInfo.subscribePlans[i].key = i; //设置key
+                        this.AppInfo.subscribePlans[i].editing = false; //是否处在编辑模式
+                    }
                     this.loading = false;
                 } else {
                     message.error('获取信息失败');
@@ -253,6 +305,55 @@ export default {
                 this.AppInfo.tags.push(this.newTag);
                 this.newTag = '';
             }
+        },
+        planEditClicked(plankey) {
+            const newData = [...this.AppInfo.subscribePlans];
+            const target = newData.filter(item => plankey === item.key)[0];
+            if (target) {
+                this.planEditInfo.newInfo = {...target}; //把行值复制到newInfo
+                target.editing = true;//开启行编辑
+                this.AppInfo.subscribePlans = newData; //
+                this.planEditInfo.editing = true; //action列的其他行禁用
+            }
+        },
+        planEditCancelClicked(plankey) {
+            const newData = [...this.AppInfo.subscribePlans];
+            const target = newData.filter(item => plankey === item.key)[0];
+            if (target) {
+                target.editing = false; //关闭行编辑
+                this.AppInfo.subscribePlans = newData; //触发更新
+                this.planEditInfo.editing = false;//恢复action列其他行的disable状态
+            }
+        },
+        planEditSaveClicked(plankey) {
+            const newData = [...this.AppInfo.subscribePlans];
+            for (let i=0; i<newData.length; ++i) {
+                if (newData[i].key === plankey) {
+                    this.updatePlanInfo(this.planEditInfo.newInfo).then(()=>{ //请求服务器更新
+                        newData[i] = {...this.planEditInfo.newInfo}; //复制新值
+                        newData[i].editing = false; //关闭行编辑
+                        this.AppInfo.subscribePlans = newData; //触发更新
+                        this.planEditInfo.editing = false; //恢复action列其他行的disable状态
+                    }); 
+                    break;
+                }
+            }
+        },
+        updatePlanInfo(info) {
+            return axios.post("/publisher/changeplaninfo", qs.stringify({
+                data: encodeURIComponent(JSON.stringify(info))
+            })).then(res => {
+                return new Promise((resolve, reject)=>{
+                    if (res.data.success) {
+                        message.success('修改已保存');
+                        resolve();
+                    } else {
+                        message.error('操作执行失败');
+                        console.error('执行updatePlanInfo时遇到错误：', res.data);
+                        reject();
+                    }
+                })
+            });
         }
     },
     created() {
